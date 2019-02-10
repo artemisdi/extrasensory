7a6704f5a39a509ce22e6ca4a97b27f2d8d6cab6
master <golimarartemisdi@gmail.com>
2019-02-10 17:38:04 +0300

    test

--- script.js
+++ script.js
@@ -72,10 +72,10 @@ function addAjax(url, code, params) {
             if (request.status === 200) {
                 if (request.response !== false) {
                     if (request.response) {
-                        let x = JSON.parse(request.response);
+                        let answer = JSON.parse(request.response);
                         // если только имена
-                        if (x.name && x.numberUser === undefined) {
-                            if (x.name.length > 11) {
+                        if (answer.name && answer.numberUser === undefined) {
+                            if (answer.name.length > 11) {
                                 console.log('больше 11 lets go');
                                 document.body.style.display = 'block';
                             }
@@ -83,80 +83,80 @@ function addAjax(url, code, params) {
                             docId('tHeadRow').innerHTML += '<td>ИГРОК</td>';
                             docId('tBodyPrestige').innerHTML = '<td>ПРЕСТИЖ</td><td></td>';
                             docId('tBodyResult').innerHTML = '<td>РЕЗУЛЬТАТ</td>';
-                            for (let i = 0; i < x.name.length; i++) {
-                                docId('tHeadRow').innerHTML += '<td>' + x.name[i] + '</td>';
+                            for (let i = 0; i < answer.name.length; i++) {
+                                docId('tHeadRow').innerHTML += '<td>' + answer.name[i] + '</td>';
                                 docId('tBodyPrestige').innerHTML += '<td id="prestige-' + i + '"></td>';
                             }
-                            for (let i = 0; i < x.name.length + 1; i++) {
+                            for (let i = 0; i < answer.name.length + 1; i++) {
                                 docId('tBodyResult').innerHTML += '<td id="result-' + i + '"><ol id="ol-' + i + '"></ol></td>';
                             }
                             docId('formOne').classList.add('disabled');
                             docId('formTwo').classList.remove('disabled');
                             // если имена и данные есть
-                        } else if (x.name && x.numberUser) {
-                            if (x.name.length > 11) {
+                        } else if (answer.name && answer.numberUser) {
+                            if (answer.name.length > 11) {
                                 document.body.style.display = 'block';
                             }
                             docId('tHeadRow').innerHTML = '<td>ИГРОКИ /<br> ДАННЫЕ</td>';
                             docId('tHeadRow').innerHTML += '<td>ИГРОК</td>';
                             docId('tBodyPrestige').innerHTML = '<td>ПРЕСТИЖ</td><td></td>';
                             docId('tBodyResult').innerHTML = '<td>РЕЗУЛЬТАТ</td>';
-                            for (let i = 0; i < x.name.length; i++) {
+                            for (let i = 0; i < answer.name.length; i++) {
                                 // console.log(x.name[i]);
-                                docId('tHeadRow').innerHTML += '<td>' + x.name[i] + '</td>';
+                                docId('tHeadRow').innerHTML += '<td>' + answer.name[i] + '</td>';
                                 docId('tBodyPrestige').innerHTML += '<td id="prestige-' + i + '"></td>';
                             }
-                            for (let i = 0; i < x.name.length + 1; i++) {
+                            for (let i = 0; i < answer.name.length + 1; i++) {
                                 docId('tBodyResult').innerHTML += '<td id="result-' + i + '"><ol id="ol-' + i + '"></ol></td>';
                             }
                             docId('window').classList.add('active');
-                            for (let i = 0; i < x.numberUser.length; i++) {
+                            for (let i = 0; i < answer.numberUser.length; i++) {
                                 let userNumber = document.getElementById('ol-0');
-                                userNumber.innerHTML += '<li>' + x.numberUser[i] + '</li>';
+                                userNumber.innerHTML += '<li>' + answer.numberUser[i] + '</li>';
                             }
                             //цикл для построения ячеек
-                            for (let j = 0; j < x.numberExtraArray.length; j++) {
+                            for (let j = 0; j < answer.numberExtraArray.length; j++) {
                                 let counter = j + 1;
                                 let extraNumber = document.getElementById("ol-" + counter);
                                 let nestedListInCell = '';
                                 //цикл для построения списков в ячейках
-                                for (let i = 0; i < x.numberExtraArray[j].numberGuessed.length; i++) {
+                                for (let i = 0; i < answer.numberExtraArray[j].numberGuessed.length; i++) {
                                     // console.log(x.numberExtraArray[j].matchId[i]);
-                                    if (x.numberExtraArray[j].matchId[i]) {
-                                        nestedListInCell += '<li class="matchId">' + x.numberExtraArray[j].numberGuessed[i] + '</li>';
+                                    if (answer.numberExtraArray[j].matchId[i]) {
+                                        nestedListInCell += '<li class="matchId">' + answer.numberExtraArray[j].numberGuessed[i] + '</li>';
                                     } else {
-                                        nestedListInCell += '<li>' + x.numberExtraArray[j].numberGuessed[i] + '</li>';
+                                        nestedListInCell += '<li>' + answer.numberExtraArray[j].numberGuessed[i] + '</li>';
                                     }
                                 }
                                 extraNumber.innerHTML = nestedListInCell;
-                                document.getElementById('prestige-' + j).innerHTML = x.numberExtraArray[j].prestige;
+                                document.getElementById('prestige-' + j).innerHTML = answer.numberExtraArray[j].prestige;
                             }
                             //добавление последнего значения пользователя в шар
-                            docId('imageUserData').innerHTML = '<span>' + x.numberUser[x.numberUser.length - 1] + '</span><div class="sprite-lip" id="spriteLip"></div>';
+                            docId('imageUserData').innerHTML = '<span>' + answer.numberUser[answer.numberUser.length - 1] + '</span><div class="sprite-lip" id="spriteLip"></div>';
                             //скрывание  и отображение окон
                             docId('formOne').classList.add('disabled');
                             docId('formTwo').classList.remove('disabled');
                             //при нажатии на кнопку загадать число
-                        } else if (x.numberExtraArray) {
-                            for (let i = 0; i < x.numberUser.length; i++) {
+                        } else if (answer.numberExtraArray) {
+                            for (let i = 0; i < answer.numberUser.length; i++) {
                                 let userNumber = document.getElementById('ol-0');
-                                userNumber.innerHTML += '<li>' + x.numberUser[x.numberUser.length - 1] + '</li>';
+                                userNumber.innerHTML += '<li>' + answer.numberUser[answer.numberUser.length - 1] + '</li>';
                                 break;
                             }
-                            for (let j = 0; j < x.numberExtraArray.length; j++) {
+                            for (let j = 0; j < answer.numberExtraArray.length; j++) {
                                 let counter = j + 1;
                                 let extraNumber = document.getElementById("ol-" + counter);
-                                if (x.numberExtraArray[j].matchId[x.numberUser.length - 1] === true) {
-                                    extraNumber.innerHTML += '<li class="matchId">' + x.numberExtraArray[j].numberGuessed[x.numberExtraArray[j].numberGuessed.length - 1] + '</li>';
+                                if (answer.numberExtraArray[j].matchId[answer.numberUser.length - 1] === true) {
+                                    extraNumber.innerHTML += '<li class="matchId">' + answer.numberExtraArray[j].numberGuessed[answer.numberExtraArray[j].numberGuessed.length - 1] + '</li>';
                                 }
                                 //проверка на совпадение, для отображения стилей
                                 else {
-                                    extraNumber.innerHTML += '<li>' + x.numberExtraArray[j].numberGuessed[x.numberExtraArray[j].numberGuessed.length - 1] + '</li>';
+                                    extraNumber.innerHTML += '<li>' + answer.numberExtraArray[j].numberGuessed[answer.numberExtraArray[j].numberGuessed.length - 1] + '</li>';
                                 }
-                                document.getElementById('prestige-' + j).innerHTML = x.numberExtraArray[j].prestige;
+                                document.getElementById('prestige-' + j).innerHTML = answer.numberExtraArray[j].prestige;
                             }
                             //добавление последнего значения пользователя в шар
-                            docId('imageUserData').innerHTML = '<span>' + x.numberUser[x.numberUser.length - 1] + '</span><div class="sprite-lip" id="spriteLip"></div>';
+                            docId('imageUserData').innerHTML = '<span>' + answer.numberUser[answer.numberUser.length - 1] + '</span><div class="sprite-lip" id="spriteLip"></div>';
                         }
                     }
                 }
